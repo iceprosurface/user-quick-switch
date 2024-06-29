3a50e97c811806801c9be0db51f6b23d4cb44058
icepro <icepro@xd.com>
2024-06-29 12:55:58 +0800

    fix: fix error when remove menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,38 @@
-import { createSignal, Match, Switch } from "solid-js";
+import { createEffect, createSignal, Match, Switch } from "solid-js";
 import { TabStorage } from "./components";
 import { SettingPage } from "./components/SettingPage";
+import { getActiveTab } from "./utils/getActiveTab";
 const enum Tab {
   Storage = "storage",
   Settings = "settings",
 }
+async function requestPermission() {
+  const activeTab = await getActiveTab();
+  if (!activeTab || !activeTab.url) {
+    return Promise.resolve(false);
+  }
+  const { promise, resolve } = Promise.withResolvers<boolean>();
+  chrome.permissions.request(
+    {
+      permissions: ["cookies", "tabs", "storage", "scripting", "contextMenus"],
+      origins: [new URL(activeTab.url).origin],
+    },
+    (granted) => {
+      if (granted) {
+        resolve(true);
+      } else {
+        resolve(false);
+      }
+    },
+  );
+  return promise;
+}
 export function App() {
   const [currentTab, setCurrentTab] = createSignal<Tab>(Tab.Storage);
+  const [hasPermission, setHasPermission] = createSignal<boolean>(false);
+  createEffect(() => {
+    requestPermission().then(setHasPermission);
+  });
   return (
     <div class="h-full">
       <Switch>
--- src/background.ts
+++ src/background.ts
@@ -1,61 +1,50 @@
 import { applyIdentity, getIdList, Identity } from "./utils/identity";
-import { debounce } from "lodash-es";
-function removeContext(name: string): Promise<void> {
-  return new Promise((resolve) => {
-    chrome.contextMenus.remove(name, () => {
-      resolve();
-    });
-  });
-}
 function createRootContextMenu(): Promise<void> {
-  return new Promise((resolve) => {
-    chrome.contextMenus.create(
-      {
-        id: "ids",
-        title: "切换身份",
-        contexts: ["all"],
-      },
-      () => {
-        resolve();
-      },
-    );
-  });
+  const { promise, resolve } = Promise.withResolvers<void>();
+  chrome.contextMenus.create(
+    {
+      id: "ids",
+      title: "切换身份",
+      contexts: ["all"],
+    },
+    () => {
+      resolve();
+    },
+  );
+  return promise;
 }
 function createContextMenu(name: string, title: string): Promise<void> {
-  return new Promise((resolve) => {
-    chrome.contextMenus.create(
-      {
-        parentId: "ids",
-        id: name,
-        title: title,
-        contexts: ["all"],
-      },
-      () => {
-        resolve();
-      },
-    );
-  });
+  const { promise, resolve } = Promise.withResolvers<void>();
+  chrome.contextMenus.create(
+    {
+      parentId: "ids",
+      id: name,
+      title: title,
+      contexts: ["all"],
+    },
+    () => {
+      resolve();
+    },
+  );
+  return promise;
 }
 async function setupContextMenu() {
-  // current tab
-  await removeContext("ids");
   await createRootContextMenu();
   const idList = await getIdList();
   for (const id of idList) {
     await createContextMenu(id, id);
   }
 }
-const debounceSetupContextMenu = debounce(setupContextMenu, 200);
-chrome.tabs.onActivated.addListener(() => debounceSetupContextMenu());
-chrome.storage.onChanged.addListener(() => debounceSetupContextMenu());
-chrome.runtime.onInstalled.addListener(() => debounceSetupContextMenu());
+
+chrome.tabs.onActivated.addListener(() => setupContextMenu());
+chrome.storage.onChanged.addListener(() => setupContextMenu());
+chrome.runtime.onInstalled.addListener(() => setupContextMenu());
 chrome.contextMenus.onClicked.addListener(async (info, tab) => {
   if (!tab) {
     return;
   }
   const id = info.menuItemId?.toString();
   const identity = (await chrome.storage.local.get(id))[id] as Identity;
-  console.log("identity", identity);
   if (!identity) {
     return;
   }
